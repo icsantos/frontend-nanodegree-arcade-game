1424844da5f16c4b0a9988d3c791f19ac35c7225
icsantos <isantos1206@gmail.com>
2015-09-09 05:29:07 -0500

    More JSDoc markups.  Add and link to minified JS files.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-/** 
+/**
  * Size of a stone, grass or water block along x-axis.
  * @constant
  * @type {number}
  * @default
  */
 var BLOCK_WIDTH = 101,
-  /** 
+  /**
    * Size of a stone, grass or water block along y-axis.
    * @constant
    * @type {number}
    * @default
    */
   BLOCK_HEIGHT = 83,
-  /** 
+  /**
    * Transparent part of a stone, grass or water block above the block image.
    * @constant
    * @type {number}
@@ -20,28 +20,28 @@ var BLOCK_WIDTH = 101,
    */
   BLOCK_HEIGHT_OFFSET = 50;
 
-/** 
+/**
  * Top of the playing field.
  * @constant
  * @type {number}
  * @default BLOCK_HEIGHT_OFFSET
  */
 var FIELD_TOP = BLOCK_HEIGHT_OFFSET,
-  /** 
+  /**
    * Left side of the playing field.
    * @constant
    * @type {number}
    * @default
    */
   FIELD_LEFT = 0,
-  /** 
+  /**
    * Bottom of the playing field.
    * @constant
    * @type {number}
    * @default
    */
   FIELD_BOTTOM = 400,
-  /** 
+  /**
    * Right side of the playing field.
    * @constant
    * @type {number}
@@ -82,23 +82,31 @@ function toggleAvatarSelection(enable) {
 // GamePiece superclass
 // --------------------------------------------------
 /**
- * A superclass of the playing pieces we can use in the game.
  * @constructor
+ * @classdesc A superclass of the playing pieces we can use in the game.  It requires
+ * the modules {@link http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js},
+ * {@link resources.js} and {@link engine.js}.
+ * @requires http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+ * @requires resources.js
+ * @requires engine.js
+ * @property {string} sprite - The URL of the image sprite.  The image/sprite uses a helper to easily load images.
+ * @property {number} height - The height of the image sprite.
+ * @property {number} width - The width of the image sprite.
+ * @property {number} x - The location of the game piece on the x-axis.
+ * @property {number} y - The location of the game piece on the y-axis.
+ * @property {object} perimeter - The boundaries of the game piece on the x-axis and y-axis.
+ * @property {number} perimeter.top - The top of the game piece.
+ * @property {number} perimeter.left - The left side of the game piece.
+ * @property {number} perimeter.bottom - The bottom of the game piece.
+ * @property {number} perimeter.right - The right side of the game piece.
  */
 var GamePiece = function () {
   'use strict';
 };
 
 /**
- * Represents a playing piece.
- * @param {object} obj - An object containing the image sprite URL, width and height.
- * @example
- * // Sample value for the obj parameter
- * {
- *    sprite: 'images/enemy-bug.png',
- *    width: 99,
- *    height: 66
- * }
+ * Method that sets the sprite, width and height properties of a game piece.
+ * @param {avatars|enemies|tokens} obj - An object containing the image sprite URL, width and height.
  * @property {string} sprite - The URL of the image sprite.  The image/sprite uses a helper to easily load images.
  * @property {number} width - The width of the image sprite.
  * @property {number} height - The height of the image sprite.
@@ -183,6 +191,26 @@ GamePiece.prototype.hit = function (object2) {
 // --------------------------------------------------
 // Enemy class
 // --------------------------------------------------
+/**
+ * Array of objects that will be used to instantiate an enemy game piece
+ * @typedef {object} enemies
+ * @type {Object[]}
+ * @property {string} sprite - The URL of the image sprite.
+ * @property {number} width - The width of the image sprite.
+ * @property {number} height - The height of the image sprite.
+ * @example
+ * var enemies = [
+ *   {
+ *     sprite: 'images/enemy-bug.png',
+ *     width: 99,
+ *     height: 66
+ *   }, {
+ *     sprite: 'images/ladybag.png',
+ *     width: 63,
+ *     height: 66
+ *   }
+ * ];
+ */
 var enemies = [
   {
     sprite: 'images/enemy-bug.png',
@@ -199,6 +227,7 @@ var enemies = [
 /**
  * @constructor
  * @extends GamePiece
+ * @property {number} speed - The initial speed of the enemy game piece.
  */
 var Enemy = function () {
   'use strict';
@@ -208,7 +237,10 @@ var Enemy = function () {
 Enemy.prototype = Object.create(GamePiece.prototype); // subclass prototype delegation
 Enemy.prototype.constructor = Enemy; // reset constructor from GamePiece to Enemy
 
-// Place enemy at one of the stone-block rows (y-axis), starting off-canvas (x-axis)
+/**
+ * Place enemy at one of the stone-block rows (y-axis), starting off-canvas (x-axis)
+ * @property {number} speed - The initial speed of the enemy game piece.
+ */
 Enemy.prototype.reset = function () {
   'use strict';
   this.piece(enemies[randomInteger(1, enemies.length) - 1]);
@@ -217,8 +249,16 @@ Enemy.prototype.reset = function () {
   this.speed = randomInteger(75, 200);
 };
 
-// Update the enemy's position
-// Parameter: dt, a time delta between ticks
+/**
+ * This method:
+ * <ul>
+ * <li>Updates the enemy's position and perimeter.
+ * <li>Checks if the enemy has walked off the playing field.
+ * <li>Checks if the enemy hit the player.
+ * </ul>
+ * @see https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+ * @param {number} dt - A time delta between ticks
+ */
 Enemy.prototype.update = function (dt) {
   'use strict';
   // Multiply any movement by the dt parameter to ensure
@@ -240,7 +280,29 @@ Enemy.prototype.update = function (dt) {
 // --------------------------------------------------
 // Player class
 // --------------------------------------------------
-// for purpose of collision detection, the height excludes the shadow
+/**
+ * Array of objects that will be used to instantiate a player game piece
+ * @typedef {object} avatars
+ * @type {Object[]}
+ * @property {string} id - The "value" of the corresponding radio button in index.html.
+ * @property {string} sprite - The URL of the image sprite.
+ * @property {number} width - The width of the image sprite.
+ * @property {number} height - The height of the image sprite. For purpose of collision detection, the height excludes the shadow.
+ * @example
+ * var avatars = [
+ *   {
+ *     id: 'char-boy',
+ *     sprite: 'images/char-boy.png',
+ *     width: 67,
+ *     height: 77
+ *   }, {
+ *     id: 'char-cat-girl',
+ *     sprite: 'images/char-cat-girl.png',
+ *     width: 68,
+ *     height: 76
+ *   }
+ * ];
+ */
 var avatars = [
   {
     id: 'char-boy',
@@ -270,6 +332,20 @@ var avatars = [
   }
 ];
 
+/**
+ * This function:
+ * <ul>
+ * <li>Constructs a GamePiece of avatars type.
+ * <li>Selects the avatar to use (if one is not specified) and uses jQuery to indicate the selected avatar.
+ * <li>Calls GamePiece.piece() to set the sprite, height and width properties.
+ * <li>Initializes the score and lives properties.
+ * </ul>
+ * @constructor
+ * @extends GamePiece
+ * @property {number} score - The number of points scored by the player.
+ * @property {number} lives - The number of lives remaining for the player.
+ * @param {string} [charid] - An optional parameter identifying the avatars.id to use. If not provided, an avatar will be randomly chosen.
+ */
 var Player = function (charId) {
   'use strict';
   GamePiece.call(this);
@@ -293,7 +369,16 @@ var Player = function (charId) {
 Player.prototype = Object.create(GamePiece.prototype); // subclass prototype delegation
 Player.prototype.constructor = Player; // reset constructor from GamePiece to Player
 
-// Place player at starting point
+/**
+ * This method:
+ * <ul>
+ * <li>Calls GamePiece.xCoord() and GamePiece.yCoord() to set starting position of the player.
+ * <li>Calls GamePiece.setPerimeter() to set the player's hit zone.
+ * <li>Initializes the tokenPoints and tokenLives properties.
+ * </ul>
+ * @property {number} tokenPoints - The number of banked points accumulated by picking up tokens.
+ * @property {number} tokenLives - The number of banked lives accumulated by picking up tokens.
+ */
 Player.prototype.reset = function () {
   'use strict';
   this.xCoord(0, 4);
@@ -303,14 +388,25 @@ Player.prototype.reset = function () {
   this.tokenLives = 0;
 };
 
-// Update the player's position
+/**
+ * This method:
+ * <ul>
+ * <li>Checks if the player has zero lives and if so, calls gameOver().
+ * <li>Checks if the player has reached the safety zone and if so, 
+ *   <ul>
+ *   <li>Updates score by calling <tt>updateScore(1 + this.tokenPoints)</tt>
+ *   <li>Updates lives by calling <tt>updateLives(this.tokenLives)</tt>
+ *   <li>Places the player back to the starting position by calling <tt>reset()</tt>.
+ *   </ul>
+ * </ul>
+ */
 Player.prototype.update = function () {
   'use strict';
   if (this.lives < 1) {
     this.gameOver();
     return 0;
   }
-  // Has the player reached the water?
+  // Has the player reached the safety zone?
   if (this.y < FIELD_TOP) {
     this.updateScore(1 + this.tokenPoints);
     this.updateLives(this.tokenLives);
@@ -320,7 +416,16 @@ Player.prototype.update = function () {
   return 1;
 };
 
-// Move player in the direction selected
+/**
+ * This method:
+ * <ul>
+ * <li>Updates the x coordinate if the player pressed the left or right movement key.
+ * <li>Updates the y coordinate if the player pressed the up or down movement key.
+ * </ul>
+ * @listens module:addEventListener~keyup
+ * @param {string} key - The key pressed by the player.
+ */
+// 
 Player.prototype.handleInput = function (key) {
   'use strict';
   switch (key) {
@@ -339,14 +444,28 @@ Player.prototype.handleInput = function (key) {
   }
 };
 
-// Update the player's score
+/**
+ * This method:
+ * <ul>
+ * <li>Adds the value specified in the parameter to the score.
+ * <li>Uses jQuery to display the score on the page.
+ * </ul>
+ * @param {number} score - The number of points to add to the player's score.
+ */
 Player.prototype.updateScore = function (score) {
   'use strict';
   this.score += score;
   $('#score').text(this.score);
 };
 
-// Update the player's lives
+/**
+ * This method:
+ * <ul>
+ * <li>Adds the value specified in the parameter to the player's lives, up to a maximum of 5.
+ * <li>Uses jQuery to display the number of lives on the page, represented by the number of visible heart images.
+ * </ul>
+ * @param {number} lives - The number of lives to add to or deduct from the player's lives.
+ */
 Player.prototype.updateLives = function (lives) {
   'use strict';
   this.lives = (this.lives + lives >= 5) ? 5 : this.lives + lives;
@@ -359,7 +478,9 @@ Player.prototype.updateLives = function (lives) {
   });
 };
 
-// No more lives
+/**
+ * This method uses jQuery to display the final score on the page.
+ */
 Player.prototype.gameOver = function () {
   'use strict';
   $('#score').text("Your Score: " + this.score);
@@ -368,6 +489,32 @@ Player.prototype.gameOver = function () {
 // --------------------------------------------------
 // Token class
 // --------------------------------------------------
+/**
+ * Array of objects that will be used to instantiate a token game piece
+ * @typedef {object} tokens
+ * @type {Object[]}
+ * @property {string} sprite - The URL of the image sprite.
+ * @property {number} width - The width of the image sprite.
+ * @property {number} height - The height of the image sprite. For purpose of collision detection, the height excludes the shadow.
+ * @property {number} points - The number of points added when the token is taken to the safety zone.
+ * @property {number} lives - The number of lives added when the token is taken to the safety zone.
+ * @example
+ * var avatars = [
+ *   {
+ *     sprite: 'images/Gem Blue.png',
+ *     width: 50,
+ *     height: 54,
+ *     points: 2,
+ *     lives: 0
+ *   }, {
+ *     sprite: 'images/Heart.png',
+ *     width: 45,
+ *     height: 45,
+ *     points: 0,
+ *     lives: 1
+ *   }
+ * ];
+ */
 var tokens = [
   {
     sprite: 'images/Gem Blue.png',
@@ -408,6 +555,11 @@ var tokens = [
   }
 ];
 
+/**
+ * This function constructs a GamePiece of tokens type.
+ * @constructor
+ * @extends GamePiece
+ */
 var Token = function () {
   'use strict';
   GamePiece.call(this);
@@ -416,7 +568,20 @@ var Token = function () {
 Token.prototype = Object.create(GamePiece.prototype); // subclass prototype delegation
 Token.prototype.constructor = Token; // reset constructor from GamePiece to Token
 
-// Token will be placed on a stone-block
+/**
+ * This method:
+ * <ul>
+ * <li>Randomly selects the token to use.
+ * <li>Calls GamePiece.piece() to set the sprite, height and width properties.
+ * <li>Calls GamePiece.xCoord() and GamePiece.yCoord() to set the position of the token.  The token will be placed on a stone-block.
+ * <li>Initializes the points, lives, delay, fadeTime and alphaDivisor properties.
+ * </ul>
+ * @property {number} points - The number of points the token is worth.
+ * @property {number} lives - The number of lives the token is worth.
+ * @property {number} delay - How long to wait before rendering the token.
+ * @property {number} fadeTime - How long it will take before the token fades completely from view.
+ * @property {number} alphaDivisor - This will be used for setting the globalAlpha before rendering the token.
+ */
 Token.prototype.reset = function () {
   'use strict';
   var obj = tokens[randomInteger(1, tokens.length) - 1];
@@ -430,7 +595,20 @@ Token.prototype.reset = function () {
   this.yCoord(1, 3);
 };
 
-// Update the token's position
+/**
+ * This method:
+ * <ul>
+ * <li>Calls GamePiece.setPerimeter() to set the token's hit zone.
+ * <li>Checks if the delay has expired and if the token was hit by the player, and if so:
+ *   <ul>
+ *   <li>Bank the points by adding token.points to player.tokenPoints.
+ *   <li>Bank the lives by adding token.lives to player.tokenLives.
+ *   <li>Prepare another token by calling the reset() method.
+ *   </ul>
+ * </ul>
+ * @property {number} tokenPoints - The number of banked points accumulated by picking up tokens.
+ * @property {number} tokenLives - The number of banked lives accumulated by picking up tokens.
+ */
 Token.prototype.update = function () {
   'use strict';
   this.setPerimeter();
@@ -441,6 +619,16 @@ Token.prototype.update = function () {
   }
 };
 
+/**
+ * This method:
+ * <ul>
+ * <li>Deducts 100 from the delay property.
+ * <li>If the delay is more than zero, it does nothing else.
+ * <li>If the delay has reached zero or below, it deducts 100 from the fadeTime property.
+ * <li>If the fadeTime is more than zero, it adjusts the globalAlpha, calls the GamePiece.prototype.render() method, then resets the globalAlpha to 1.
+ * <li>If the fadeTime has reached zero or below, it calls the token's reset() method.
+ * </ul>
+ */
 // Draw the token on the screen
 Token.prototype.render = function () {
   'use strict';
@@ -460,17 +648,32 @@ Token.prototype.render = function () {
 // --------------------------------------------------
 // Instantiate objects
 // --------------------------------------------------
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/**
+ * Array of instances of the Enemy class
+ * @type {Enemy[]}
+ */
 var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()],
+  /**
+   * An instance of the Player class
+   * @type {Player}
+   */
   player = new Player(),
+  /**
+   * An instance of the Token class
+   * @type {Token}
+   */
   token = new Token();
 
 // --------------------------------------------------
 // Event Listeners
 // --------------------------------------------------
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/**
+ * Event listener that listens for key presses and sends the keys to 
+ * the Player.handleInput() method.
+ * @event module:addEventListener~keyup
+ * @function addEventListener
+ * @global
+ */
 document.addEventListener('keyup', function (e) {
   'use strict';
   var allowedKeys = {
